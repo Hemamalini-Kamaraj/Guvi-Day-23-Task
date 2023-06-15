685acf0bfaeaebbfe2d2f7b1078bfbf708799577
Hemamalini-Kamaraj <hemamalini.ahkm@gmail.com>
2023-06-15 11:26:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import { Component } from "react";
+// import React, { useState } from 'react'
 
-// Class component
-class App extends Component {
-  render() {
+// function App() {
+// const [counter, setCounter] = useState(0);
 
-    return <div>Hello</div>
-  }
+// setTimeout(
+//   () => setCounter(counter+1) ,1000)
+
+//   console.log("rendering..", counter)
+
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState } from 'react'
+
+const Display = (props) => {
+  return(
+    <div>{props.counter}</div>
+  )
 }
 
-export default App;
+function Button (props) {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+function App() {
+
+  const [counter,setCounter] = useState(0);
+  const [showButton, setShowButton] = useState(true);
+
+  const handleIncrementClick = () => setCounter(counter+1);
+  
+  const handleDecrementClick = () => setCounter(counter-1);
+
+  const handleZeroClick = () => setCounter(0);
+
+  return (
+    <div>
+      <Display counter= {counter} />
+      <Button text="plus" handleClick = {handleIncrementClick}/>&nbsp;
+      <Button text="minus" handleClick = {handleDecrementClick}/>&nbsp;
+      <Button text="zero" handleClick = {handleZeroClick}/>
+      {/* <button onClick={handleIncrementClick}> Plus </button>&nbsp;
+      <button onClick={handleDecrementClick}> Minus </button>&nbsp;
+      <button onClick={handleZeroClick}> Zero </button> */}
+      
+    </div>
+  )
+}
+
+export default App
