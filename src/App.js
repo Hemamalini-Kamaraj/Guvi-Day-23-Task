79d097a983b57336d861ad001b6778d4478dffad
Hemamalini-Kamaraj <hemamalini.ahkm@gmail.com>
2023-06-19 20:00:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import "./App.css";
+import MenuBar from "./Components/MenuBar";
+import SearchBar from "./Components/SearchBar";
+import Title from "./Components/Title";
+import { StatusCardList } from "./Components/StatusCard";
+import OverViewChart from "./Components/OverViewChart";
+import { ProjectsList } from "./Components/Projects";
+import Illustrations from "./Components/Illustrations";
+import { ColoredCardsList } from "./Components/ColoredCards";
+import DevelopmentApproach from "./Components/DevelopmentApproach";
+import Copyright from "./Components/Copyright"
+
 
 function App() {
   return (
-    <div>
-        
-    </div>
+    <>
+        <div className="App">
+            <div className="main1">
+                <MenuBar />
+            </div>
+            <div className="main2">
+                <SearchBar />
+             </div>
+             <div className="main3">
+                <Title />
+                <StatusCardList />
+                <OverViewChart />
+                <div className="main4">
+                  <ProjectsList />
+                  <Illustrations />
+                </div>
+                <div className="main5">
+                  <ColoredCardsList />
+                  <DevelopmentApproach />
+                </div>
+                <Copyright />
+              </div>
+        </div>
+    </>
   )
 }
 
-export default App
+export default App;
---
+++ src/Components/Copyright.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Copyright = () => {
+  return <div className="copyright">Copyright © Your Website 2023</div>;
+};
+
+export default Copyright;
